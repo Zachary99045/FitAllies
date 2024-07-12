269d01fd13f76fed06ece72073b02e128453c594
Zachary99045 <zachary99045@gmail.com>
2024-07-12 23:49:50 +0800

    header disappear

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,11 +3,11 @@ import {Tabs} from "expo-router";
 export default () =>{
     return(
         <Tabs>
-            <Tabs.Screen name="home" />
-            <Tabs.Screen name="chat" />
-            <Tabs.Screen name="meet" />
-            <Tabs.Screen name="point" />
-            <Tabs.Screen name="profile" />
+            <Tabs.Screen name="home" options = {{headerShown: false}}/>
+            <Tabs.Screen name="chat" options = {{headerShown: false}}/>
+            <Tabs.Screen name="meet" options = {{headerShown: false}}/>
+            <Tabs.Screen name="point" options = {{headerShown: false}}/>
+            <Tabs.Screen name="profile" options = {{headerShown: false}}/>
         </Tabs>
     ); 
 };
